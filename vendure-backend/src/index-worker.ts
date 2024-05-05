83f6b1de0cc8725f6fcf32de663767edafab6aab
Rasmus Puls <rasmus@triggerz.net>
2024-05-05 20:32:39 +0200

    attempt to populate db on seed

--- vendure-backend/src/index-worker.ts
+++ vendure-backend/src/index-worker.ts
@@ -1,11 +1,19 @@
-import { bootstrapWorker } from '@vendure/core';
+import path from 'path';
+import { bootstrap, bootstrapWorker, VendureConfig } from '@vendure/core';
+import { populate } from '@vendure/core/cli';
 import { config } from './vendure-config';
 import { dbSeeded, DbConnectionOptions } from './db-setup';
+import { INestApplicationContext } from '@nestjs/common/interfaces/nest-application-context.interface';
+const initialDataPath = path.join(require.resolve('@vendure/create'), '../assets/initial-data.json');
 
+async function bootstrapServer(config: any): Promise<INestApplicationContext> {
+    const app = await bootstrap(config);
+    return app;
+}
 
 (async () => {
     const dbAlreadySeeded = await dbSeeded(config.dbConnectionOptions as DbConnectionOptions);
-    const updatedConfig = {
+    const updatedConfig: VendureConfig = {
         ...config,
         dbConnectionOptions: {
             ...config.dbConnectionOptions,
@@ -13,13 +21,20 @@ import { dbSeeded, DbConnectionOptions } from './db-setup';
         },
     };
     console.log('index-worker.ts', updatedConfig.dbConnectionOptions);
+
+    if (!dbAlreadySeeded) {
+        console.log('Populating database with initial data...');
+        await populate(
+            () => bootstrapServer(updatedConfig),
+            require(initialDataPath),
+        );
+        console.log('Database populated with initial data');
+    }
     bootstrapWorker(updatedConfig)
-    .then(worker => {
-        console.log('Worker started');
-        worker.startJobQueue();
-        console.log('after startJobQueue');
-    })
-    .catch(err => {
-        console.log(err);
-    });
+        .then(worker => {
+            worker.startJobQueue();
+        })
+        .catch(err => {
+            console.log(err);
+        });
 })();
