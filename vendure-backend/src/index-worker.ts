f1f214f33407569953c364b34bd9e7ad266d3db6
Rasmus Puls <rasmus@triggerz.net>
2024-05-05 22:24:03 +0200

    try new way of populating db

--- vendure-backend/src/index-worker.ts
+++ vendure-backend/src/index-worker.ts
@@ -1,7 +1,10 @@
-import { bootstrapWorker } from '@vendure/core';
+import path from 'path';
+import { bootstrap, bootstrapWorker } from '@vendure/core';
 import { config } from './vendure-config';
 import { dbSeeded, DbConnectionOptions } from './db-setup';
+import { populate } from '@vendure/core/cli'; 
 
+const initialDataPath = path.join(require.resolve('@vendure/create'), '../assets/initial-data.json');
 
 (async () => {
     const dbAlreadySeeded = await dbSeeded(config.dbConnectionOptions as DbConnectionOptions);
@@ -12,6 +15,20 @@ import { dbSeeded, DbConnectionOptions } from './db-setup';
             synchronize: !dbAlreadySeeded,
         },
     };
+
+    populate(
+        () => bootstrap(updatedConfig),
+        require(initialDataPath)
+            .then((app: any) => app.close())
+            .then(
+                () => process.exit(0),
+                (err: any) => {
+                    console.log('Populating db failed...', err);
+                    process.exit(1);
+                },
+            )
+    );
+
     console.log('index-worker.ts', updatedConfig.dbConnectionOptions);
     bootstrapWorker(updatedConfig)
     .then(worker => {
