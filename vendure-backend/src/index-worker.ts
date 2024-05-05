5a1f55d5c0b91699de4d628166eef8585a9223b5
Rasmus Puls <rasmus@triggerz.net>
2024-05-05 22:53:15 +0200

    better runtime control

--- vendure-backend/src/index-worker.ts
+++ vendure-backend/src/index-worker.ts
@@ -2,43 +2,31 @@ import path from 'path';
 import { bootstrap, bootstrapWorker } from '@vendure/core';
 import { config } from './vendure-config';
 import { dbSeeded, DbConnectionOptions } from './db-setup';
-import { populate } from '@vendure/core/cli'; 
+import { populate } from '@vendure/core/cli';
 
 const initialDataPath = path.join(require.resolve('@vendure/create'), '../assets/initial-data.json');
 
 (async () => {
-    const dbAlreadySeeded = await dbSeeded(config.dbConnectionOptions as DbConnectionOptions);
-    const updatedConfig = {
-        ...config,
-        dbConnectionOptions: {
-            ...config.dbConnectionOptions,
-            synchronize: !dbAlreadySeeded,
-        },
-    };
+  const dbAlreadySeeded = await dbSeeded(config.dbConnectionOptions as DbConnectionOptions);
+  const updatedConfig = {
+    ...config,
+    dbConnectionOptions: {
+      ...config.dbConnectionOptions,
+      synchronize: !dbAlreadySeeded,
+    },
+  };
 
-    console.log('before populate');
-    await populate(
-        () => bootstrap(updatedConfig),
-        require(initialDataPath)
-    ).then((app: any) => {
-        console.log('Populating db succeeded! closing app...');
-        app.close();
-    })
-    .then(
-        () => process.exit(0),
-        (err: any) => {
-            console.log('Populating db failed...', err);
-            process.exit(1);
-        },
-    );
+  console.log('before populate');
+  try {
+    const app = await populate(() => bootstrap(config), require(initialDataPath));
+    await app.close();
     console.log('after populate');
 
     console.log('index-worker.ts', updatedConfig.dbConnectionOptions);
-    bootstrapWorker(updatedConfig)
-    .then(worker => {
-        worker.startJobQueue();
-    })
-    .catch(err => {
-        console.log(err);
-    });
+    const worker = await bootstrapWorker(updatedConfig);
+    worker.startJobQueue();
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
 })();
